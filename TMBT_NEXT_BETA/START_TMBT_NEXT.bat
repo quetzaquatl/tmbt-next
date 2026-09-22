dec832a16a496b41c4574cd429ea800c011f8389
quetzaquatl <lukas.mazur96@gmail.com>
2026-09-22 14:49:21 +0200

    Bump UI to 0.9.66 seasonality context

--- TMBT_NEXT_BETA/START_TMBT_NEXT.bat
+++ TMBT_NEXT_BETA/START_TMBT_NEXT.bat
@@ -7,7 +7,7 @@ set "TMBT_NEXT_PORT=8510"
 
 echo.
 echo =========================================
-echo TMBT NEXT BETA 0.9.65 CONTEXT QUICK
+echo TMBT NEXT BETA 0.9.66 SEASONALITY CONTEXT
 echo =========================================
 echo Workspace: %TMBT_WORKSPACE%
 echo URL: http://127.0.0.1:%TMBT_NEXT_PORT%
