70e0e54ad677817e9272cdd6e5fabd06966956d3
quetzaquatl <lukas.mazur96@gmail.com>
2026-09-22 11:20:20 +0200

    Bump build to 0.9.65 context quick

--- TMBT_NEXT_BETA/server_active.py
+++ TMBT_NEXT_BETA/server_active.py
@@ -215,7 +215,7 @@ def context_locked_models():
 
 
 core.normalize_models = context_locked_models
-core.APP_VERSION = "0.9.64-beta-optimizer-safe"
+core.APP_VERSION = "0.9.65-beta-context-quick"
 
 
 class Handler(ready.Handler):
